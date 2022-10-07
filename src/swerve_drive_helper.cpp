cdfb4f4a688db20d08770833b92bf294f3541475
Michael Todd <eltodd@gmail.com>
2022-10-07 17:30:34 +0000

    Print cleanup

--- src/swerve_drive_helper.cpp
+++ src/swerve_drive_helper.cpp
@@ -18,15 +18,13 @@ static double smallest_traversal(double angle, double target_angle)
 std::ostream& operator<<(std::ostream& os, const std::vector<std::pair<geometry::Pose, geometry::Twist>>& value)
 {
     std::stringstream s;
-    s << "---------------------------------" << std::endl;
-    s << "Pose:Twist Vector" << std::endl;
     for (std::vector<std::pair<geometry::Pose, geometry::Twist>>::const_iterator i = value.begin();
          i != value.end();
          i++)
     {
-        s << (*i).first << " : " << (*i).second << std::endl;
+        s << "Pose: " << (*i).first << "Twist: " << (*i).second;
+        s << "---" << std::endl;
     }
-    s << "---------------------------------" << std::endl;
     os << s.str();
     return os;
 }
@@ -34,15 +32,13 @@ std::ostream& operator<<(std::ostream& os, const std::vector<std::pair<geometry:
 std::ostream& operator<<(std::ostream& os, const std::vector<geometry::Transform>& value)
 {
     std::stringstream s;
-    s << "---------------------------------" << std::endl;
-    s << "Transform Vector" << std::endl;
     for (std::vector<geometry::Transform>::const_iterator i = value.begin();
          i != value.end();
          i++)
     {
-        s << (*i) << std::endl;
+        s << (*i);
+        s << "---" << std::endl;
     }
-    s << "---------------------------------" << std::endl;
     os << s.str();
     return os;
 }
@@ -52,26 +48,24 @@ std::vector<std::pair<geometry::Pose, geometry::Twist>> calculate_swerve_outputs
     std::vector<geometry::Transform> wheel_transforms,
     float projection_time_s)
 {
-    std::stringstream inputs;
-    inputs << "calculate_swerve_outputs_internal" << std::endl;
-    inputs << "Inputs: " << std::endl;
-    inputs << "Desired Twist: " << desired_twist << std::endl;
-    inputs << "Wheel Transforms: " << wheel_transforms << std::endl;
-    inputs << "Projection Time s:" << projection_time_s << std::endl;
-    inputs << std::endl;
-    ROS_INFO("%s", inputs.str().c_str());
+    std::stringstream log;
+    log << std::endl;
+    log << "---------------------------------" << std::endl;
+    log << "calculate_swerve_outputs_internal" << std::endl;
+    log << "----------" << std::endl;
+    log << "Inputs: " << std::endl;
+    log << "Desired Twist: " << std::endl << desired_twist << std::endl;
+    log << "Wheel Transforms: " << std::endl << wheel_transforms;
+    log << "Projection Time s:" << projection_time_s << std::endl;
+    log << "----------" << std::endl;
 
     std::vector<std::pair<geometry::Pose, geometry::Twist>> results;
 
     geometry::Pose robot_initial_pose;
     geometry::Pose robot_projected_pose = robot_initial_pose.twist(desired_twist, projection_time_s);
 
-    std::stringstream k1;
-    k1 << "robot_initial_pose: " << robot_initial_pose << std::endl;
-    ROS_INFO("%s", k1.str().c_str());
-    std::stringstream k2;
-    k2 << "robot_projected_pose: " << robot_projected_pose << std::endl;
-    ROS_INFO("%s", k2.str().c_str());
+    log << "robot_initial_pose: " << robot_initial_pose;
+    log << "robot_projected_pose: " << robot_projected_pose;
 
     float largest_hypot = 0;
 
@@ -79,20 +73,16 @@ std::vector<std::pair<geometry::Pose, geometry::Twist>> calculate_swerve_outputs
         i != wheel_transforms.end();
         i++)
     {
+        // log << "----------" << std::endl;
         geometry::Pose wheel_initial_pose = robot_initial_pose.transform(*i);
         geometry::Pose wheel_projected_pose = robot_projected_pose.transform(*i);
         std::stringstream s0;
-        s0 << "I: " << (*i) << std::endl;
-        ROS_INFO("%s", s0.str().c_str());
+        // log << "I: " << (*i);
 
         geometry::Transform wheel_transformation = wheel_initial_pose.get_Transform(wheel_projected_pose);
 
-        std::stringstream s1;
-        s1 << "wheel_transformation: " << wheel_transformation << std::endl;
-        ROS_INFO("%s", s1.str().c_str());
-        std::stringstream s2;
-        s2 << "wheel_projected_pose: " << wheel_projected_pose << std::endl;
-        ROS_INFO("%s", s2.str().c_str());
+        // log << "wheel_transformation: " << wheel_transformation;
+        // log << "wheel_projected_pose: " << wheel_projected_pose;
 
         largest_hypot = std::max(wheel_transformation.linear.norm(), largest_hypot);
         float wheel_end_yaw = wheel_transformation.get_Rotation_To().yaw();
@@ -103,15 +93,15 @@ std::vector<std::pair<geometry::Pose, geometry::Twist>> calculate_swerve_outputs
         float normal_smallest_traversal = smallest_traversal(initial_yaw, wheel_end_yaw);
         float mirror_smallest_traversal = smallest_traversal(mirrored_initial_yaw, wheel_end_yaw);
 
-        ROS_INFO("initial_yaw: %f\n", initial_yaw);
-        ROS_INFO("wheel_end_yaw: %f\n", wheel_end_yaw);
-        ROS_INFO("mirrored_initial_yaw: %f\n", mirrored_initial_yaw);
-        ROS_INFO("normal_smallest_traversal: %f\n", normal_smallest_traversal);
-        ROS_INFO("mirror_smallest_traversal: %f\n", mirror_smallest_traversal);
+        // log << "initial_yaw: " << initial_yaw << std::endl;
+        // log << "wheel_end_yaw: " << wheel_end_yaw << std::endl;
+        // log << "mirrored_initial_yaw: " << mirrored_initial_yaw << std::endl;
+        // log << "normal_smallest_traversal: " << normal_smallest_traversal << std::endl;
+        // log << "mirror_smallest_traversal: " << mirror_smallest_traversal << std::endl;
 
         float smallest_overall_traversal = std::fabs(normal_smallest_traversal) < std::fabs(mirror_smallest_traversal) ? normal_smallest_traversal : mirror_smallest_traversal;
 
-        ROS_INFO("Smallest Traversal %f\n", smallest_overall_traversal);
+        // log << "Smallest Traversal" << smallest_overall_traversal << std::endl;
         geometry::Rotation smallest_traversal_pose;
         smallest_traversal_pose.roll(0);
         smallest_traversal_pose.pitch(0);
@@ -141,11 +131,13 @@ std::vector<std::pair<geometry::Pose, geometry::Twist>> calculate_swerve_outputs
         }
     }
 
-    std::stringstream outputs;
-    outputs << "Outputs:" << std::endl;
-    outputs << results << std::endl;
-    outputs << "End calculate_swerve_outputs_internal" << std::endl;
-    ROS_INFO("%s", outputs.str().c_str());
+    log << "----------" << std::endl;
+    log << "Outputs:" << std::endl;
+    log << results << std::endl;
+    log << "End calculate_swerve_outputs_internal" << std::endl;
+    log << "---------------------------------";
+
+    ROS_INFO("%s", log.str().c_str());
 
     return results;
 }
@@ -216,7 +208,6 @@ std::vector<std::pair<geometry_msgs::Pose, geometry_msgs::Twist>> calculate_swer
 
     std::vector<std::pair<geometry::Pose, geometry::Twist>> raw_results;
     raw_results = calculate_swerve_outputs_internal(desired_twist_, wheel_transforms_, projection_time_s);
-    ROS_INFO("escaped function\n");
     std::vector<std::pair<geometry_msgs::Pose, geometry_msgs::Twist>> results;
 
     for (std::vector<std::pair<geometry::Pose, geometry::Twist>>::iterator i = raw_results.begin();
@@ -228,10 +219,9 @@ std::vector<std::pair<geometry_msgs::Pose, geometry_msgs::Twist>> calculate_swer
                 (geometry::to_msg((*i).first), geometry::to_msg((*i).second));
         results.push_back(entry);
     }
-    ROS_INFO("Publishing results\n");
-    std::stringstream s;
-    s << results;
-    ROS_INFO("Results\n%s", s.str().c_str());
+    // std::stringstream s;
+    // s << results;
+    // ROS_INFO("Results\n%s", s.str().c_str());
 
     return results;
 }
