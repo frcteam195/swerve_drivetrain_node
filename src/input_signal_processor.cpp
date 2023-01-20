c9e95ce13e17ec2f0e1c5aa6bc4d1b6b80465b90
Michael Todd <eltodd@gmail.com>
2023-01-20 04:47:43 +0000

    Be lazy properly

--- src/input_signal_processor.cpp
+++ src/input_signal_processor.cpp
@@ -30,7 +30,7 @@ geometry::Twist perform_heading_stabilization(geometry::Twist twist, geometry::P
 
 	double target_angular_velocity = twist.angular.yaw();
 
-	geometry::Transform robot_pose;
+	geometry::Transform robot_pose = get_robot_transform();
 	drivetrain_diagnostics.actual_heading = ck::math::rad2deg(robot_pose.angular.yaw());
 
 	if (enable_absolute_heading)
