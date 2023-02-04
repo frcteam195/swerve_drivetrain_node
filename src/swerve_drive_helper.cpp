02deb9cddb551796b7df1bed39de82f37634215a
Michael Todd <eltodd@gmail.com>
2023-02-04 21:09:39 +0000

    Cleanup

--- src/swerve_drive_helper.cpp
+++ src/swerve_drive_helper.cpp
@@ -1,10 +1,10 @@
 #include "swerve_drive_helper.hpp"
+#include "config_params.hpp"
 #include <ck_utilities/CKMath.hpp>
 #include <sstream>
 #include <ros/ros.h>
 #include <iostream>
 
-// #define DETAILED_LOGGING
 // #define PURE_MATH
 
 double smallest_traversal(double angle, double target_angle)
@@ -53,28 +53,10 @@ std::vector<std::pair<geometry::Pose, geometry::Twist>> calculate_swerve_outputs
     std::vector<geometry::Transform> wheel_transforms,
     float projection_time_s)
 {
-#ifdef DETAILED_LOGGING
-    std::stringstream log;
-    log << std::endl;
-    log << "---------------------------------" << std::endl;
-    log << "calculate_swerve_outputs_internal" << std::endl;
-    log << "----------" << std::endl;
-    log << "Inputs: " << std::endl;
-    log << "Desired Twist: " << std::endl << desired_twist << std::endl;
-    log << "Wheel Transforms: " << std::endl << wheel_transforms;
-    log << "Projection Time s:" << projection_time_s << std::endl;
-    log << "----------" << std::endl;
-#endif
-
     std::vector<std::pair<geometry::Pose, geometry::Twist>> results;
 
     geometry::Pose robot_initial_pose;
     geometry::Pose robot_projected_pose = robot_initial_pose.twist(desired_twist, projection_time_s);
-
-#ifdef DETAILED_LOGGING
-    log << "robot_initial_pose: " << robot_initial_pose;
-    log << "robot_projected_pose: " << robot_projected_pose;
-#endif
 
     float largest_speed_demand = 0;
 
@@ -82,18 +64,10 @@ std::vector<std::pair<geometry::Pose, geometry::Twist>> calculate_swerve_outputs
         i != wheel_transforms.end();
         i++)
     {
-#ifdef DETAILED_LOGGING
-        log << "----------" << std::endl;
-#endif
         geometry::Pose wheel_initial_pose = robot_initial_pose.transform(*i);
         geometry::Pose wheel_projected_pose = robot_projected_pose.transform(*i);
 
         geometry::Transform wheel_transformation = wheel_initial_pose.get_Transform(wheel_projected_pose);
-
-#ifdef DETAILED_LOGGING
-        log << "wheel_transformation: " << wheel_transformation;
-        log << "wheel_projected_pose: " << wheel_projected_pose;
-#endif
 
         float wheel_end_yaw = ck::math::normalize_to_2_pi(wheel_transformation.get_Rotation_To().yaw());
 
@@ -103,32 +77,16 @@ std::vector<std::pair<geometry::Pose, geometry::Twist>> calculate_swerve_outputs
         float normal_smallest_traversal = smallest_traversal(initial_yaw, wheel_end_yaw);
         float mirror_smallest_traversal = smallest_traversal(mirrored_initial_yaw, wheel_end_yaw);
 
-#ifdef DETAILED_LOGGING
-        log << "initial_yaw: " << initial_yaw << std::endl;
-        log << "wheel_end_yaw: " << wheel_end_yaw << std::endl;
-        log << "mirrored_initial_yaw: " << mirrored_initial_yaw << std::endl;
-        log << "normal_smallest_traversal: " << normal_smallest_traversal << std::endl;
-        log << "mirror_smallest_traversal: " << mirror_smallest_traversal << std::endl;
-#endif
-
         float smallest_overall_traversal = std::fabs(normal_smallest_traversal) < std::fabs(mirror_smallest_traversal) ? normal_smallest_traversal : mirror_smallest_traversal;
         bool flipped = std::fabs(mirror_smallest_traversal) < std::fabs(normal_smallest_traversal);
 
-#ifdef DETAILED_LOGGING
-        log << "Smallest Traversal" << smallest_overall_traversal << std::endl;
-#endif
         geometry::Rotation smallest_traversal_pose;
-        smallest_traversal_pose.roll(0);
-        smallest_traversal_pose.pitch(0);
         smallest_traversal_pose.yaw(ck::math::normalize_to_2_pi(initial_yaw + smallest_overall_traversal));
 
         std::pair<geometry::Pose, geometry::Twist> wheel_result;
         wheel_result.first.position = wheel_projected_pose.position;
         wheel_result.first.orientation = smallest_traversal_pose;
-        wheel_result.second.linear.setZero();
         wheel_result.second.linear[0] = wheel_transformation.linear.norm() / projection_time_s * (flipped ? -1.0 : 1.0);
-        wheel_result.second.angular.roll(0);
-        wheel_result.second.angular.pitch(0);
         wheel_result.second.angular.yaw(smallest_overall_traversal / projection_time_s);
 
         if (std::abs(wheel_result.second.linear[0]) > largest_speed_demand)
@@ -144,23 +102,12 @@ std::vector<std::pair<geometry::Pose, geometry::Twist>> calculate_swerve_outputs
          i++)
     {
         if (std::abs(largest_speed_demand) > 0.001 &&
-            largest_speed_demand > 3.5 && // these 3.5s should be the kinematic limit, not hard coded, will fix later MGT
-            std::abs(3.5) > 0.001)
+            largest_speed_demand > config_params::robot_max_fwd_vel)
         {
-            float ratio = std::abs(3.5 / largest_speed_demand);
+            float ratio = std::abs(config_params::robot_max_fwd_vel / largest_speed_demand);
             (*i).second.linear[0] *= ratio;
         }
     }
-
-#ifdef DETAILED_LOGGING
-    log << "----------" << std::endl;
-    log << "Outputs:" << std::endl;
-    log << results << std::endl;
-    log << "End calculate_swerve_outputs_internal" << std::endl;
-    log << "---------------------------------";
-    ROS_DEBUG("%s", log.str().c_str());
-
-#endif
 
     return results;
 }
