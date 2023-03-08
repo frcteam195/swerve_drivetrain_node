36ab21d07bc47c76bc69dda4066f051f810354c8
Michael Todd <eltodd@gmail.com>
2023-03-08 03:13:44 +0000

    Whoops x 5

--- src/swerve_drivetrain_node.cpp
+++ src/swerve_drivetrain_node.cpp
@@ -121,13 +121,13 @@ void apply_x_mode()
             }
             case 3:
             {
-                pose.orientation.yaw(ck::math::deg2rad(315));
+                pose.orientation.yaw(ck::math::deg2rad(45));
                 break;
             }
             default:
             case 4:
             {
-                pose.orientation.yaw(ck::math::deg2rad(45));
+                pose.orientation.yaw(ck::math::deg2rad(315));
                 break;
             }
         }
