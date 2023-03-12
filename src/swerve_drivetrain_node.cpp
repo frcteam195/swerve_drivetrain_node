f97f5feae162c01fd143b0fdf54071656b034308
Tom <TSLourenco@WPI.edu>
2023-03-12 19:45:59 +0000

    Added X mode.

--- src/swerve_drivetrain_node.cpp
+++ src/swerve_drivetrain_node.cpp
@@ -42,13 +42,13 @@ bool run_once = true;
 
 void update_motor_transforms()
 {
-	//Update swerve steering transforms
-	for (int i = 0; i < config_params::robot_num_wheels; i++)
-	{
-		geometry::Rotation rotation;
-		rotation.yaw(ck::math::normalize_to_2_pi(ck::math::deg2rad(motor_map[config_params::steering_motor_ids[i]].sensor_position * 360.0)));
-		wheel_transforms[i].angular = rotation;
-	}
+    //Update swerve steering transforms
+    for (int i = 0; i < config_params::robot_num_wheels; i++)
+    {
+        geometry::Rotation rotation;
+        rotation.yaw(ck::math::normalize_to_2_pi(ck::math::deg2rad(motor_map[config_params::steering_motor_ids[i]].sensor_position * 360.0)));
+        wheel_transforms[i].angular = rotation;
+    }
 }
 
 void kill_motors()
@@ -62,27 +62,27 @@ void kill_motors()
         mS->set( Motor::Control_Mode::PERCENT_OUTPUT, 0, 0 );
     }
 
-	for (size_t i = 0; i < drive_motors.size(); i++)
-	{
-		drivetrain_diagnostics.modules[i].target_steering_angle_deg = ck::math::rad2deg(ck::math::normalize_to_2_pi(motor_map[config_params::steering_motor_ids[i]].sensor_position * 2.0 * M_PI));
-		drivetrain_diagnostics.modules[i].actual_steering_angle_deg = ck::math::rad2deg(ck::math::normalize_to_2_pi(motor_map[config_params::steering_motor_ids[i]].sensor_position * 2.0 * M_PI));
-		drivetrain_diagnostics.modules[i].target_speed_m_s = 0;
-		drivetrain_diagnostics.modules[i].actual_speed_m_s = motor_map[config_params::drive_motor_ids[i]].sensor_velocity * (0.1016 * M_PI) / 60.0;
-	}
+    for (size_t i = 0; i < drive_motors.size(); i++)
+    {
+        drivetrain_diagnostics.modules[i].target_steering_angle_deg = ck::math::rad2deg(ck::math::normalize_to_2_pi(motor_map[config_params::steering_motor_ids[i]].sensor_position * 2.0 * M_PI));
+        drivetrain_diagnostics.modules[i].actual_steering_angle_deg = ck::math::rad2deg(ck::math::normalize_to_2_pi(motor_map[config_params::steering_motor_ids[i]].sensor_position * 2.0 * M_PI));
+        drivetrain_diagnostics.modules[i].target_speed_m_s = 0;
+        drivetrain_diagnostics.modules[i].actual_speed_m_s = motor_map[config_params::drive_motor_ids[i]].sensor_velocity * (0.1016 * M_PI) / 60.0;
+    }
 }
 
 void apply_robot_twist(geometry::Twist desired_twist, bool useDeadband=true)
 {
-	double linear_deadband = 0.2;
-	double angular_deadband = 20;
+    double linear_deadband = 0.2;
+    double angular_deadband = 20;
 
-	if (!useDeadband)
-	{
-		linear_deadband = 0.1;
-		angular_deadband = 10;
-	}
+    if (!useDeadband)
+    {
+        linear_deadband = 0.0;
+        angular_deadband = 10;
+    }
 
-	if (std::abs(desired_twist.linear.norm()) > linear_deadband ||
+    if (std::abs(desired_twist.linear.norm()) > linear_deadband ||
         std::abs(desired_twist.angular.yaw()) > ck::math::deg2rad(angular_deadband))
     {
         std::vector<std::pair<geometry::Pose, geometry::Twist>> sdo = calculate_swerve_outputs(desired_twist, wheel_transforms);
@@ -96,7 +96,7 @@ void apply_robot_twist(geometry::Twist desired_twist, bool useDeadband=true)
 
 void publish_diagnostic_data()
 {
-	diagnostics_publisher->publish(drivetrain_diagnostics);
+    diagnostics_publisher->publish(drivetrain_diagnostics);
 }
 
 void apply_x_mode()
@@ -141,111 +141,118 @@ void apply_x_mode()
 
 void process_swerve_logic()
 {
-	static int8_t frame_counter = 0;
-	frame_counter ++;
-	frame_counter = frame_counter % 100;
+    static int8_t frame_counter = 0;
+    frame_counter ++;
+    frame_counter = frame_counter % 100;
 
-	update_motor_transforms();
+    update_motor_transforms();
 
-	if(frame_counter % 20 == 0)
-	{
-		publish_motor_links();
-	}
+    if(frame_counter % 20 == 0)
+    {
+        publish_motor_links();
+    }
 
     if (frame_counter % 5 == 0)
     {
         publish_robot_base_tf();
     }
 
-	geometry::Twist desired_robot_twist;
+    geometry::Twist desired_robot_twist;
 
-	switch (robot_status.robot_state)
-	{
-		case ck_ros_base_msgs_node::Robot_Status::AUTONOMOUS:
-		{
-			set_brake_mode(true);
-			desired_robot_twist = get_twist_from_auto();
+    switch (robot_status.robot_state)
+    {
+        case ck_ros_base_msgs_node::Robot_Status::AUTONOMOUS:
+        {
+            set_brake_mode(true);
+            desired_robot_twist = get_twist_from_auto();
             if (auto_control.x_mode)
             {
                 apply_x_mode();
             }
             else
             {
-	            apply_robot_twist(desired_robot_twist, false);
+                apply_robot_twist(desired_robot_twist, false);
             }
-		}
-		break;
-		case ck_ros_base_msgs_node::Robot_Status::TELEOP:
-		{
-			set_brake_mode(hmi_signals.drivetrain_brake);
-			desired_robot_twist = get_twist_from_HMI();
-            apply_robot_twist(desired_robot_twist);
-		}
-		break;
-		default:
-		{
-			run_once = true;
+        }
+        break;
+        case ck_ros_base_msgs_node::Robot_Status::TELEOP:
+        {
+            set_brake_mode(hmi_signals.drivetrain_brake);
+            desired_robot_twist = get_twist_from_HMI();
+            if (hmi_signals.drivetrain_xmode)
+            {
+                apply_x_mode();
+            }
+            else
+            {
+                apply_robot_twist(desired_robot_twist);
+            }
+        }
+        break;
+        default:
+        {
+            run_once = true;
             kill_motors();
-		}
-		break;
-	}
+        }
+        break;
+    }
 
-	geometry::Transform robot_pose = get_robot_transform();
-	drivetrain_diagnostics.actual_heading = ck::math::rad2deg(robot_pose.angular.yaw());
+    geometry::Transform robot_pose = get_robot_transform();
+    drivetrain_diagnostics.actual_heading = ck::math::rad2deg(robot_pose.angular.yaw());
 
-	publishOdometryData();
-	publish_diagnostic_data();
+    publishOdometryData();
+    publish_diagnostic_data();
 }
 
 void motor_status_callback(const ck_ros_base_msgs_node::Motor_Status& motor_status_)
 {
-	std::map<uint16_t, ck_ros_base_msgs_node::Motor_Info> receipt_map;
-	for(auto &i : motor_status_.motors)
-	{
-		receipt_map[i.id] = i;
-	}
+    std::map<uint16_t, ck_ros_base_msgs_node::Motor_Info> receipt_map;
+    for(auto &i : motor_status_.motors)
+    {
+        receipt_map[i.id] = i;
+    }
 
-	motor_map = receipt_map;
+    motor_map = receipt_map;
 
-	process_swerve_logic();
+    process_swerve_logic();
 }
 
 void robot_status_callback(const ck_ros_base_msgs_node::Robot_Status& robot_status_)
 {
-	robot_status = robot_status_;
+    robot_status = robot_status_;
 }
 
 void hmi_signals_callback(const ck_ros_msgs_node::HMI_Signals& hmi_signals_)
 {
-	hmi_signals = hmi_signals_;
+    hmi_signals = hmi_signals_;
 }
 
 void auto_control_callback(const ck_ros_msgs_node::Swerve_Drivetrain_Auto_Control& auto_control_)
 {
-	auto_control = auto_control_;
+    auto_control = auto_control_;
 }
 
 int main(int argc, char **argv)
 {
-	ros::init(argc, argv, "drivetrain");
+    ros::init(argc, argv, "drivetrain");
 
-	ros::NodeHandle n;
+    ros::NodeHandle n;
 
-	node = &n;
-	if (!config_params::init_params(n))
-	{
-		ROS_ERROR("NOT ALL SWERVE PARAMETERS SET - NODE TERMINATING");
-		return 1;
-	}
+    node = &n;
+    if (!config_params::init_params(n))
+    {
+        ROS_ERROR("NOT ALL SWERVE PARAMETERS SET - NODE TERMINATING");
+        return 1;
+    }
 
-	init_swerve_motors();
+    init_swerve_motors();
 
-	static ros::Subscriber motor_status_subscriber = node->subscribe("/MotorStatus", 1, motor_status_callback, ros::TransportHints().tcpNoDelay());
-	static ros::Subscriber robot_status_subscriber = node->subscribe("/RobotStatus", 1, robot_status_callback, ros::TransportHints().tcpNoDelay());
-	static ros::Subscriber hmi_signals_subscriber = node->subscribe("/HMISignals", 1, hmi_signals_callback, ros::TransportHints().tcpNoDelay());
-	static ros::Subscriber auto_signals_subscriber = node->subscribe("/SwerveAutoControl", 1, auto_control_callback, ros::TransportHints().tcpNoDelay());
-	ros::Publisher diagnostics_publisher_ = node->advertise<ck_ros_msgs_node::Swerve_Drivetrain_Diagnostics>("/SwerveDiagnostics", 10);
-	diagnostics_publisher = &diagnostics_publisher_;
-	ros::spin();
-	return 0;
+    static ros::Subscriber motor_status_subscriber = node->subscribe("/MotorStatus", 1, motor_status_callback, ros::TransportHints().tcpNoDelay());
+    static ros::Subscriber robot_status_subscriber = node->subscribe("/RobotStatus", 1, robot_status_callback, ros::TransportHints().tcpNoDelay());
+    static ros::Subscriber hmi_signals_subscriber = node->subscribe("/HMISignals", 1, hmi_signals_callback, ros::TransportHints().tcpNoDelay());
+    static ros::Subscriber auto_signals_subscriber = node->subscribe("/SwerveAutoControl", 1, auto_control_callback, ros::TransportHints().tcpNoDelay());
+    ros::Publisher diagnostics_publisher_ = node->advertise<ck_ros_msgs_node::Swerve_Drivetrain_Diagnostics>("/SwerveDiagnostics", 10);
+    diagnostics_publisher = &diagnostics_publisher_;
+    ros::spin();
+    return 0;
 }
